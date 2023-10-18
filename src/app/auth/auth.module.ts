f1937e19596285286341e13d577d30a8cff67b59
Declan <hung34atpvti@gmail.com>
2023-10-18 15:38:42 +0700

    jwt constants

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -11,7 +11,7 @@ import { AuthResolver } from './auth.resolver';
     JwtModule.register({
       global: true,
       secret: jwtConstants.secret,
-      signOptions: { expiresIn: '300s' },
+      signOptions: { expiresIn: jwtConstants.expiredTime },
     }),
   ],
   providers: [AuthService, AuthResolver],
